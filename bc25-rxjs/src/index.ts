0bf3ab70ac9d1f0b1f06fab8f600c4fb0d3746c5
pidp1 <pedro.imparato@gmail.com>
2022-10-31 15:05:39 -0300

    innitial commit

---
+++ bc25-rxjs/src/index.ts
@@ -0,0 +1,19 @@
+import path from 'path'
+
+const filePaths: string[] = [
+  path.join(__dirname, 'files', 'app_1.txt'),
+  path.join(__dirname, 'files', 'app_2.txt'),
+  path.join(__dirname, 'files', 'app_3.txt'),
+  path.join(__dirname, 'files', 'app_4.txt'),
+  path.join(__dirname, 'files', 'estilo_1.css'),
+  path.join(__dirname, 'files', 'estilo_2.css'),
+  path.join(__dirname, 'files', 'estilo_3.css'),
+  path.join(__dirname, 'files', 'estilo_4.css'),
+  path.join(__dirname, 'files', 'estrutura_1.html'),
+  path.join(__dirname, 'files', 'estrutura_2.html'),
+  path.join(__dirname, 'files', 'estrutura_3.html'),
+  path.join(__dirname, 'files', 'estrutura_4.html')
+]
+
+const isCSS = /^((.|#){0,1}(\w+-{0,1})+\s*{(\s*(\w+-{0,1})+:\s*(\w+\s*)+;\s*)+\s*}\s*)/i
+const isHTML = /^<!DOCTYPE html>/i
